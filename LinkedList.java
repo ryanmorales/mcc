71f6ccc3e06d4a1b8c9550fd5438de3e452635e8
ryanmorales <ryan.morales14@gmail.com>
2025-04-07 01:17:31 +0800

    Create LinkedList.java

---
+++ LinkedList.java
@@ -0,0 +1,43 @@
+
+public class LinkedList {
+	
+	Node head;
+	
+	public void insertAtEnd(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        Node temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        temp.next = newNode;
+    }
+
+    public void delete(int data) {
+        if (head == null) return;
+        if (head.data == data) {
+            head = head.next;
+            return;
+        }
+        Node temp = head;
+        while (temp.next != null && temp.next.data != data) {
+            temp = temp.next;
+        }
+        if (temp.next != null) {
+            temp.next = temp.next.next;
+        }
+    }
+
+    public void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+}

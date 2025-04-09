3e00e70cec514bd98e073dcdbc02c4883a871efd
ryanmorales <ryan.morales14@gmail.com>
2025-04-10 05:08:12 +0800

    Create BlockingQueueExample.java

---
+++ BlockingQueueExample.java
@@ -0,0 +1,26 @@
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+public class BlockingQueueExample {
+
+  public static void main(String[] args) throws InterruptedException {
+    
+    BlockingQueue<String> queue = new ArrayBlockingQueue<>(2);
+    queue.put("John");
+    queue.put("Jane");
+    System.out.println("BlockingQueue: " + queue);
+
+
+    // Adding an element (will block if queue is full)
+    queue.put("Doe"); // This will block since the queue is full
+
+    // Accessing the head of the queue
+    String head = queue.peek();
+    System.out.println("Head of BlockingQueue: " + head);
+
+    // Removing elements from the queue
+    String removedElement = queue.take();
+    System.out.println("Removed Element: " + removedElement);
+    System.out.println("BlockingQueue after removal: " + queue);
+ 
+  }
+}
